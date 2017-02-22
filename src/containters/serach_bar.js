abd5919e1d6b4f2a4ad8cfc1baa89df9250bf2b1
Rogério Toledo <rotoledojr@gmail.com>
2017-02-22 15:06:55 -0300

    [SearchBar] Form Submit Handler

--- src/containters/serach_bar.js
+++ src/containters/serach_bar.js
@@ -11,10 +11,14 @@ export default class SearchBar extends Component {
   onInputChange(event){
     this.setState({term: event.target.value});
   }
+  onFormSubmit(event){
+    event.preventDefault();
+  }
 
   render(){
     return (
-      <form className="input-group">
+      <form
+        className="input-group" onSubmit={this.onFormSubmit}>
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
