665321ca3b6c7de0be481c873a8ccd9e56ac9b7f
Rogério Toledo <rotoledojr@gmail.com>
2017-02-22 14:48:10 -0300

    Search bar initial setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import SearchBar from '../containters/serach_bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
