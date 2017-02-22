f1a2122527317a61014f8e244a5ac8007cb3f7d3
Rogério Toledo <rotoledojr@gmail.com>
2017-02-22 16:17:35 -0300

    Mapping Props to a Render Helper

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containters/search_bar';
+import WeatherList from '../containters/weather_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
