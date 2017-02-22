b6ff9dc978fbfa9742935d291af4b80160ce8d35
Rogério Toledo <rotoledojr@gmail.com>
2017-02-22 15:35:47 -0300

    Ajax Requests with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '417a4b28daf664d29af93f61f17af840';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},br`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
